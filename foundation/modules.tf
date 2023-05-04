cd673b364b28ce8258c7ad01a3a52887c0d3a8bf
msamend <marc.samendinger@mercedes-benz.com>
2023-05-04 18:27:24 +0200

    Add terraform_provider name

--- foundation/modules.tf
+++ foundation/modules.tf
@@ -1,14 +1,16 @@
 resource "spacelift_module" "tags" {
-  branch       = "main"
-  description  = "Tagging module"
-  repository   = "spacelift-poc"
-  project_root = "modules/tags"
+  branch             = "main"
+  description        = "Tagging module"
+  repository         = "spacelift-poc"
+  project_root       = "modules/tags"
+  terraform_provider = "tags"
 }
 
 resource "spacelift_module" "meta" {
-  branch       = "main"
-  description  = "meta module"
-  repository   = "spacelift-poc"
-  project_root = "modules/meta"
+  branch             = "main"
+  description        = "meta module"
+  repository         = "spacelift-poc"
+  project_root       = "modules/meta"
+  terraform_provider = "meta"
 }
 
