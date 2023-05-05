73838fd8f513017acfe1bc960585d4c074ab13a9
msamend <marc.samendinger@mercedes-benz.com>
2023-05-05 09:38:09 +0200

    Add terraform validate and fmt before planning phase

--- foundation/stack.tf
+++ foundation/stack.tf
@@ -30,5 +30,6 @@ resource "spacelift_stack" "dev" {
   labels               = ["managed", "depends-on:${data.spacelift_current_stack.this.id}", "feature:add_plan_pr_comment"]
   runner_image         = "public.ecr.aws/spacelift/runner-terraform:azure-future"
   enable_local_preview = true
+  before_plan          = ["terraform fmt -check", "terraform validate"]
 }
 
