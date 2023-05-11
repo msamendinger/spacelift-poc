5aed7f01f97224950dd7cb90ecf677d25141e2ea
Marc Samendinger <git@samendinger.de>
2023-05-11 17:28:51 +0200

    Add context azure-dev
    Make azure-dev state accessible

---
+++ foundation/contexts.tf
@@ -0,0 +1,9 @@
+resource "spacelift_context" "azure-env" {
+  description = "Azure environment needed to deploy resources"
+  name        = "azure-env"
+}
+
+resource "spacelift_context_attachment" "azure-env" {
+  context_id = spacelift_context.azure-env.id
+  stack_id   = spacelift_stack.dev.id
+}
--- foundation/stack.tf
+++ foundation/stack.tf
@@ -2,10 +2,10 @@ data "spacelift_current_stack" "this" {}
 
 resource "spacelift_stack" "stack1" {
   name                            = "stack1"
+  repository                      = "spacelift-poc"
   autodeploy                      = true
   terraform_external_state_access = true
   # Source code.
-  repository   = "spacelift-poc"
   branch       = "main"
   project_root = "stack1"
   labels       = ["managed", "depends-on:${data.spacelift_current_stack.this.id}", "feature:add_plan_pr_comment"]
@@ -23,13 +23,15 @@ resource "spacelift_stack" "stack2" {
 }
 
 resource "spacelift_stack" "dev" {
-  name                 = "azure-dev"
-  repository           = "spacelift-poc"
-  branch               = "main"
-  project_root         = "env/dev"
-  labels               = ["managed", "depends-on:${data.spacelift_current_stack.this.id}", "feature:add_plan_pr_comment"]
-  runner_image         = "public.ecr.aws/spacelift/runner-terraform:azure-future"
-  enable_local_preview = true
-  before_plan          = ["terraform fmt -check", "terraform validate"]
+  name                            = "azure-dev"
+  repository                      = "spacelift-poc"
+  autodeploy                      = true
+  terraform_external_state_access = true
+  branch                          = "main"
+  project_root                    = "env/dev"
+  labels                          = ["managed", "depends-on:${data.spacelift_current_stack.this.id}", "feature:add_plan_pr_comment"]
+  runner_image                    = "public.ecr.aws/spacelift/runner-terraform:azure-future"
+  enable_local_preview            = true
+  before_plan                     = ["terraform fmt -check", "terraform validate"]
 }
 
