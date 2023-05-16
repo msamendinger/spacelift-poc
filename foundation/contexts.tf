d8e50a43dead66c5cd657d56526e20e7356dba39
Marc Samendinger <git@samendinger.de>
2023-05-16 21:51:47 +0200

    Fix context definition

--- foundation/contexts.tf
+++ foundation/contexts.tf
@@ -10,6 +10,6 @@ resource "spacelift_context_attachment" "azure-env" {
 
 resource "spacelift_context_attachment" "foundation" {
   context_id = spacelift_context.azure-env.id
-  stack_id   = spacelift_stack.foundation.id
+  stack_id   = data.spacelift_current_stack.this.id
 }
 
