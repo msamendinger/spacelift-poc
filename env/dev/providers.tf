6054a3fb72e8618a6c4bffeee3ab66e4155d5469
Marc Samendinger <git@samendinger.de>
2023-05-23 23:59:35 +0200

    Fix azurerm provider block, oidc_true

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -1,9 +1,8 @@
 
 provider "azurerm" {
-  features {
-    use_oidc             = true
-    oidc_token_file_path = "/mnt/workspace/spacelift.oidc"
-  }
+  features {}
+  use_oidc             = true
+  oidc_token_file_path = "/mnt/workspace/spacelift.oidc"
 }
 
 provider "spacelift" {}
