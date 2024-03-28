1e72860ac0927968899311047ac5f6b47e1f944e
Marc Samendinger <git@samendinger.de>
2024-03-28 14:44:48 +0100

    Spacelift verification

--- stack1/outputs.tf
+++ stack1/outputs.tf
@@ -2,7 +2,7 @@ output "hello_world" {
   value = "hello world!"
 }
 
-output "hello_dog" {
-  value = "hello dog!"
+output "hello_spacelift" {
+  value = "spacelift-domain-verification=4684t8vi"
 }
 
